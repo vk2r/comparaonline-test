b5b20a3c7c18280b9c0b1fc165b8366a3b747ced
vk3r <theadwareman@gmail.com>
2020-08-23 01:46:06 -0400

    :rotating_light: Updated tests

--- specs/after.js
+++ specs/after.js
@@ -4,22 +4,32 @@ const { products } = require('../src/data');
 // Models
 const { CarInsurance } = require('../src/models');
 
+// Array of days
+const days = Array.apply(null, { length: 31 }).map(Number.call, Number);
+
 describe('After 30 Days', function () {
-  it('print results', function () {
 
-    const print = function (day, insurance) {
-      if (day === 0) console.log('OMGHAI!');
-      console.log(`-------- day ${day} --------`);
-      console.log('name, sellIn, price');
-      insurance.forEach(product => {
-        console.log(`${product.name}, ${product.sellIn}, ${product.price}`)
-      });
-      console.log('');
-    };
+  function print (day, insurance) {
+    if (day === 0) console.log('OMGHAI!');
+    console.log(`-------- day ${day} --------`);
+    console.log('name, sellIn, price');
+    insurance.forEach(product => {
+      console.log(`${product.name}, ${product.sellIn}, ${product.price}`)
+    });
+    console.log('');
+  }
 
-    for (let day = 0; day <= 30; day += 1) {
+  it('print results without super degrade', function () {
+    days.forEach(day => {
       const insurance = new CarInsurance(products);
-      print(day, insurance.updatePrice(day));
-    }
+      print(day, insurance.updateDataFromDay(day));
+    });
+  });
+
+  it('print results with super degrade', function () {
+    days.forEach(day => {
+      const insurance = new CarInsurance(products);
+      print(day, insurance.updateDataFromDay(day, true));
+    });
   });
 });
